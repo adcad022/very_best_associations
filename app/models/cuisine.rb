abb560b0e6f00e841e9022d97a9652d176c6aaef
adcad022 <averchot@chicagobooth.edu>
2015-08-19 10:43:40 -0500

    Update

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -2,6 +2,6 @@ class Cuisine < ActiveRecord::Base
 
 validates :name, :presence => true, :uniqueness => true
 
-has_many :dishes
+has_many :dishes, :dependent => :destroy
 
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,7 +2,7 @@ class Venue < ActiveRecord::Base
 
 validates :name, :uniqueness => { :scope => :address }
 
-validates :neighborhood_id :presence => true
+validates :neighborhood_id, :presence => true
 
 belongs_to :neighborhood
 
